5ea8924193bc06522b330e24abe01ca195a9462e
vasilecarp <vasilecarpconsulting@gmail.com>
2023-09-24 13:31:07 +0300

    All working fine ```

--- videos/admin.py
+++ videos/admin.py
@@ -9,7 +9,7 @@ class VideoAllAdmin(admin.ModelAdmin):
     list_display = ['title', 'id', 'state', 'video_id', 'is_published']
     search_fields = ['title']
     list_filter = ['state', 'active']
-    readonly_fields = ['id', 'is_published']
+    readonly_fields = ['id', 'is_published', 'publish_timestamp']
     class Meta:
         model = VideoAllProxy
 
