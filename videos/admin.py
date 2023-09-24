9982ff7a5e23ccd46d0bea6ee5f79ea2647c795e
vasilecarp <vasilecarpconsulting@gmail.com>
2023-09-24 13:25:50 +0300

    Updated

--- videos/admin.py
+++ videos/admin.py
@@ -6,9 +6,9 @@ from django.http.request import HttpRequest
 from .models import VideoAllProxy, VideoPublishedProxy
 
 class VideoAllAdmin(admin.ModelAdmin):
-    list_display = ['title', 'id', 'video_id', 'is_published']
+    list_display = ['title', 'id', 'state', 'video_id', 'is_published']
     search_fields = ['title']
-    list_filter = ['active']
+    list_filter = ['state', 'active']
     readonly_fields = ['id', 'is_published']
     class Meta:
         model = VideoAllProxy
--- videos/models.py
+++ videos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Video(models.Model):
     class VideoStateOptions(models.TextChoices):
@@ -16,10 +17,18 @@ class Video(models.Model):
     # timestamp = models.DateTimeField(auto_now_add=True)
     # updated = models.DateTimeField(auto_now=True)
     state = models.CharField(max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
-    # publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
+    publish_timestamp = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
     @property
     def is_published(self):
         return self.active
+    
+    def save(self, *args, **kwargs):
+        if self.state == self.VideoStateOptions.PUBLISH and self.publish_timestamp is None:            
+            print('save as timestamp for published')
+            self.publish_timestamp = timezone.now()
+        elif self.state == self.VideoStateOptions.DRAFT:
+            self.publish_timestamp = None
+        super().save(*args, **kwargs)
 
 class VideoAllProxy(Video):
     class Meta:
